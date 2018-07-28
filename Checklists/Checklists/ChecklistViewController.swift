cf3d25210711459946bd9cc250aaeadebc2073aa
Matthew Tuazon <matttuazon106@gmail.com>
2018-07-28 17:44:34 -0400

    added cell, made 100 cells in total and allowed for every 5 cells to always be different through modulo.

--- Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -19,7 +19,29 @@ class ChecklistViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100 //returns number of rows inputted
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        if indexPath.row % 5 == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row % 5 == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "Learn iOS development"
+        } else if indexPath.row % 5 == 3 {
+            label.text = "Practice Volleyball"
+        } else if indexPath.row % 5 == 4 {
+            label.text = "Eat some whip cream"
+        }
+        
+        return cell
+    }
 
 }
 
