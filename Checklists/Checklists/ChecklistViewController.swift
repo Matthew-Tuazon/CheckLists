291c73967b8f29cbf8da15389516e6c953489fc3
Matthew Tuazon <matttuazon106@gmail.com>
2018-07-29 15:27:22 -0400

    first five rows can be checked on/off no problem.

--- Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ChecklistViewController: UITableViewController {
 
+    let row0text = "Walk the dog"
+    let row1text = "Brush Teeth"
+    let row2text = "Learn iOS"
+    let row3text = "Volleyball Practice"
+    let row4text = "Eat Chicken"
+    
+    var row0checked = false
+    var row1checked = false
+    var row2checked = false
+    var row3checked = false
+    var row4checked = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -27,7 +39,26 @@ class ChecklistViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
         
         if let cell = tableView.cellForRow(at: indexPath){
-            if cell.accessoryType == .none { //if no checkmark next to item, when tapped, put checkmark, else checkmarked already, take out checkmark
+          var isChecked = false
+            
+            if indexPath.row == 0 {
+                row0checked = !row0checked
+                isChecked = row0checked
+            } else if indexPath.row == 1 {
+                row1checked = !row1checked
+                isChecked = row1checked
+            } else if indexPath.row == 2 {
+                row2checked = !row2checked
+                isChecked = row2checked
+            } else if indexPath.row == 3 {
+                row3checked = !row3checked
+                isChecked = row3checked
+            } else if indexPath.row == 4 {
+                row4checked = !row4checked
+                isChecked = row4checked
+            }
+            
+            if isChecked {
                 cell.accessoryType = .checkmark
             } else {
                 cell.accessoryType = .none
@@ -42,18 +73,43 @@ class ChecklistViewController: UITableViewController {
         
         let label = cell.viewWithTag(1000) as! UILabel
         if indexPath.row % 5 == 0 {
-            label.text = "Walk the dog"
+            label.text = row0text
         } else if indexPath.row % 5 == 1 {
-            label.text = "Brush my teeth"
+            label.text = row1text
         } else if indexPath.row % 5 == 2 {
-            label.text = "Learn iOS development"
+            label.text = row2text
         } else if indexPath.row % 5 == 3 {
-            label.text = "Practice Volleyball"
+            label.text = row3text
         } else if indexPath.row % 5 == 4 {
-            label.text = "Eat some whip cream"
+            label.text = row4text
         }
         
+        configureCheckmark(for: cell, at: indexPath)
         return cell
+    }
+    
+    func configureCheckmark (for cell: UITableViewCell, at indexPath: IndexPath) {
+        var isChecked = false
+        
+        if indexPath.row == 0 {
+            isChecked = row0checked
+        } else if indexPath.row == 1 {
+            isChecked = row1checked
+        } else if indexPath.row == 2 {
+            isChecked = row2checked
+        } else if indexPath.row == 3 {
+            isChecked = row3checked
+        } else if indexPath.row == 4 {
+            isChecked = row4checked
+        }
+        
+        if isChecked {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        
+        
     }
 
 }
